cdacde30ffa01bddaa95a4986688adced3d2f85f
tiennh-0236 <nguyen.hung.tien@sun-asterisk.com>
2023-07-17 17:22:30 +0700

    add config mysql, create entity department

---
+++ department-service/src/main/java/theshy/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package theshy.departmentservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import theshy.departmentservice.entity.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
