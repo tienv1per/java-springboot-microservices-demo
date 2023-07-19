77dd884a3e036025dc2700b2dc4f51a8cccdc25b
tienv1per <tienv1perds@gmail.com>
2023-07-19 12:24:19 +0700

    add load balancing for 2 instances of Department Service using Spring Cloud LoadBalancer

--- employee-service/src/main/java/theshy/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/theshy/employeeservice/service/APIClient.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import theshy.employeeservice.dto.DepartmentDTO;
 
 // OpenFeign auto create implementation for this interface => no need to rewrite
-@FeignClient(url = "http://localhost:8080/", value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080/", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     // build get department rest API
     @GetMapping("api/departments/{departmentCode}")
